f7dd3d74ff3dea07e36818846140607792e6e80f
Zach Bakewell <you@example.com>
2023-02-06 14:32:01 -0600

    edit

--- 3_simple_exercises.py
+++ 3_simple_exercises.py
@@ -1,6 +1,7 @@
 # 1) print out the value for the key 'history' using the dictionary below
 
-
+# class is the only key
+#figure out how to do nested dictionaries 
 sampleDict = { 
    "class":{ 
       "student":{ 
@@ -14,14 +15,15 @@ sampleDict = {
 }
 
 
-
+score = sampleDict["class"]["student"]["marks"]["history"]
+print(score)
 
 # 2) Add 2 inches to the son's height.
 
 dict={"son's name": "Lucas", "son's eyes": "green", "son's height": 32, "son's weight": 25}
 
-
-
+dict["son's height"] += 2
+print(dict)
 
 
 # 3) Given a Python dictionary, Change Brad’s salary to 8500
@@ -33,7 +35,8 @@ sampleDict = {
 }
 
 
-
+sampleDict["emp3"]['salary']=8500
+print(sampleDict)
 
 
 
@@ -42,4 +45,5 @@ sampleDict = {
 
 dict={"name": "Plato", "country": "Ancient Greece", "born": -427, "teacher": "Socrates", "student": "Aristotle"}
 
-
+dict["work"] = ["Apology", "Phaedo", "Republic", "Symposium"]
+print(dict)
